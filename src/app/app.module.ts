c1a12bcd6153028cf2202a3b189b5f19dc202a1a
Joanna R <jvonrr@gmail.com>
2022-03-09 12:07:09 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+
+ import { ApolloModule,APOLLO_OPTIONS } from "apollo-angular";
+ import { HttpLink } from 'apollo-angular/http';
+ import { InMemoryCache } from '@apollo/client/core';
+ import { HttpHeaders } from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ApolloModule
+  ],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: (httpLink: HttpLink) => {
+        return {
+          cache: new InMemoryCache(),
+          link:  httpLink.create({
+            uri: 'https://api.github.com/graphql',
+            headers: new HttpHeaders({
+              "Authorization": "Bearer "+"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",
+            })
+              // headers: {
+              //   'Authorization': ` token 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`
+              // }
+          })
+        };
+      },
+      deps: [HttpLink]
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
